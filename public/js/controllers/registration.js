93d53ca8ed519b261ac3e1622ff5963a0943e5c0
Ethan Fetsko <ethan.fetsko@mobiwm.com>
2016-07-05 17:57:23 -0400

    Added login/logout + google and facebook buttons.

--- public/js/controllers/registration.js
+++ public/js/controllers/registration.js
@@ -1,67 +1,24 @@
 myApp.controller('RegistrationController', 
-  ['$scope', function($scope){
+  ['$scope', 'Authentication', function($scope, Authentication){
     $scope.login = function() {
-      $scope.message = "Welcome " + $scope.user.email;
+      Authentication.login($scope.user);
+    };
+    
+    $scope.logout = function() {
+      Authentication.logout();
     };
     
     $scope.register = function() {
-      firebase.auth().createUserWithEmailAndPassword($scope.user.email, $scope.user.password)
-        .then(function(){
-          $scope.message = "Thanks for registering!";
-          $scope.$apply();
-      })
-        .catch(function(error) {
-        // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          $scope.message = errorMessage;
-        
-          console.log($scope.message);
-        // ...
-        $scope.$apply();
-        });
+      Authentication.register($scope.user);
     };
     
-    $scope.google = function() {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+    $scope.googleAuth = function() {
+      Authentication.googleAuth($scope.user);
     };
     
-    $scope.facebook = function() {
-      var provider = new firebase.auth.FacebookAuthProvider();
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+    $scope.facebookAuth = function() {
+      Authentication.facebookAuth($scope.user);
     };
-    
     
     
 }]);
